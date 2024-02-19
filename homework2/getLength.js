43e1390fd98187e9c369d1dd6d2abff95dc4a08f
Anton Liubimov <ant.liubimov@gmail.com>
2024-02-19 06:29:12 +0000

    ASTONJS-2 [feat]: Добавить условие для size

--- homework2/getLength.js
+++ homework2/getLength.js
@@ -1,6 +1,8 @@
 const getLength = (value) => {
   if (value && value.length !== undefined) {
     console.log(value.length);
+  } else if (value && value.size !== undefined) {
+    console.log(value.size);
   } else {
     console.log(0);
   }
